794b8ff9566d4519225b2b0c1785b7163c7a7da6
jeongdei <jeongdei@users.noreply.github.com>
2016-07-28 15:12:10 +0530

    textview 수정

--- app/src/main/java/cse/knu/kr/deagu/beaconsecurityapp/BeaconActivity.java
+++ app/src/main/java/cse/knu/kr/deagu/beaconsecurityapp/BeaconActivity.java
@@ -41,7 +41,7 @@ public abstract class BeaconActivity extends AppCompatActivity implements Beacon
         setContentView(R.layout.activity_main);
 
         bm=BeaconManager.getInstanceForApplication(this);
-        //tv=(TextView)findViewById(R.id.TextView);
+        tv=(TextView)findViewById(R.id.Textview);
 
         bm.getBeaconParsers().add(new BeaconParser().setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25"));
 
